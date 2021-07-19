67c7192966178119ac15bfc1f57566203b983276
Halí V <halivelazquez@outlook.com>
2021-07-18 22:09:53 -0500

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		Hala
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@keyframes foo {
+	to {
+		background: red;
+	}
+}
+
+#selector {
+	animation: foo 2s;
+}
+</style>
